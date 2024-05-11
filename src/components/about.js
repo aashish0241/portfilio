48f706e43f0034fbab3bba9f5f3124f4660d2c19
aashish0241 <timsinaaashish6@gmail.com>
2024-05-11 17:16:22 +0545

    chnaged 2024

--- src/components/about.js
+++ src/components/about.js
@@ -1,13 +1,18 @@
 import React from "react";
-import Aashish  from "../assets/aashish.jpg"
-import AashishJersery from "../assets/aashishjersey.jpg"
-import Back from "../assets/back.jpg"
+import Aashish from "../assets/aashish.jpg";
+import AashishJersery from "../assets/aashishjersey.jpg";
+import Back from "../assets/back.jpg";
+import { useLocation} from "react-router-dom";
 
 const About1 = () => {
+  const url = useLocation();
+
   return (
     <>
       <section className="overflow-hidden pt-20 pb-12 pl-3 lg:pt-[120px]  bg-[#453952] lg:pb-[90px] dark:bg-dark">
-        <h1 className="flex justify-center items-center m-1 text-6xl font-bold">About   <span className="text-yellow-800 p-2">Me</span></h1>
+        <h1 className="flex justify-center items-center m-1 text-6xl font-bold">
+          About <span className="text-yellow-800 p-2">Me</span>
+        </h1>
         <div className="container mx-auto">
           <div className="flex flex-wrap items-center justify-between -mx-4">
             <div className="w-full px-4 lg:w-6/12">
@@ -15,26 +20,18 @@ const About1 = () => {
                 <div className="w-full px-3 sm:px-4 xl:w-1/2">
                   <div className="py-3 sm:py-4">
                     <img
-                      src={ AashishJersery}
+                      src={AashishJersery}
                       alt=""
                       className="w-full rounded-2xl"
                     />
                   </div>
                   <div className="py-3 sm:py-4">
-                    <img
-                      src={Back}
-                      alt=""
-                      className="w-full rounded-2xl"
-                    />
+                    <img src={Back} alt="" className="w-full rounded-2xl" />
                   </div>
                 </div>
                 <div className="w-full px-3 sm:px-4 xl:w-1/2">
                   <div className="relative z-10 my-4">
-                    <img
-                      src={Aashish}
-                      alt=""
-                      className="w-full rounded-2xl"
-                    />
+                    <img src={Aashish} alt="" className="w-full rounded-2xl" />
                     <span className="absolute -right-7 -bottom-7 z-[-1]">
                       <svg
                         width={134}
@@ -613,25 +610,29 @@ const About1 = () => {
             <div className="w-full px-4 lg:w-1/2 xl:w-5/12">
               <div className="mt-10 lg:mt-0">
                 <h2 className="mb-5 text-4xl font-bold text-dark text-blue-500 sm:text-[40px]/[48px]">
-                Mern Stack Developer
+                  Mern Stack Developer
                 </h2>
-                <p className="mb-5  text-white  text-xl 
-                 text-body-color dark:text-dark-6">
-                My journey in the world of development began with a 
-                curiosity to understand how technology shapes our lives. 
-                
+                <p
+                  className="mb-5  text-white  text-xl 
+                 text-body-color dark:text-dark-6"
+                >
+                  My journey in the world of development began with a curiosity
+                  to understand how technology shapes our lives.
                 </p>
                 <p className="mb-8   text-white  text-xl text-body-color dark:text-dark-6">
-                Starting with basic coding exercises, I quickly found my passion 
-                in frontend development – the art of 
-                crafting visually appealing 
-                and user-friendly interfaces that bridge the gap between users and technology.
+                  Starting with basic coding exercises, I quickly found my
+                  passion in frontend development – the art of crafting visually
+                  appealing and user-friendly interfaces that bridge the gap
+                  between users and technology.
                 </p>
                 <a href="/about">
-                <button className='bg-dark dark:bg-dark-2 m-3 border-dark dark:border-dark-2 border rounded-full inline-flex items-center justify-center py-3 px-7 text-center text-base sm:text-lg lg:text-xl font-medium text-white hover:bg-body-color hover:border-body-color disabled:bg-gray-3 disabled:border-gray-3 disabled:text-dark-5'>
-            Learn More
-          </button>
-          </a>
+                {url.pathname !== "/about" && (
+    <button className="bg-dark dark:bg-dark-2 m-3 border-dark dark:border-dark-2 border rounded-full inline-flex items-center justify-center py-3 px-7 text-center text-base sm:text-lg lg:text-xl font-medium text-white hover:bg-body-color hover:border-body-color disabled:bg-gray-3 disabled:border-gray-3 disabled:text-dark-5">
+        Learn More
+    </button>
+)}
+
+                </a>
               </div>
             </div>
           </div>
--- src/components/skills.js
+++ src/components/skills.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   FaHtml5,
   FaCss3Alt,
@@ -11,25 +11,25 @@ import {
   FaGithub,
   FaFigma,
   FaChrome,
-} from 'react-icons/fa';
+} from "react-icons/fa";
 
 const skills = [
-  { name: 'HTML', icon: FaHtml5, color: '#eb4034' },
-  { name: 'CSS', icon: FaCss3Alt, color: '#0000FF' },
-  { name: 'JavaScript', icon: FaJs, color: '#FFFF00' },
-  { name: 'Node.js', icon: FaNode, color: '#008000' },
-  { name: 'React.js', icon: FaReact, color: '#ADD8E6' },
-  { name: 'MongoDB', icon: FaDatabase, color: '#008000' },
-  { name: 'Tailwind CSS', icon: FaCode, color: '#00FFFF' },
-  { name: 'Redux.js', icon: FaCode, color: '#800080' },
-  { name: 'Express', icon: FaCode, color: '#808080' },
-  { name: 'TypeScript', icon: FaCode, color: '#0000FF' },
-  { name: 'VS Code', icon: FaCode, color: '#0000FF' },
-  { name: 'Git', icon: FaGit, color: '#FF0000' },
-  { name: 'GitHub', icon: FaGithub, color: '#800080' },
-  { name: 'Netlify', icon: FaCode, color: '#000000' },
-  { name: 'Figma', icon: FaFigma, color: '#FFC0CB' },
-  { name: 'Chrome', icon: FaChrome, color: '#FF0000' },
+  { name: "HTML", icon: FaHtml5, color: "#eb4034" },
+  { name: "CSS", icon: FaCss3Alt, color: "#0000FF" },
+  { name: "JavaScript", icon: FaJs, color: "#FFFF00" },
+  { name: "Node.js", icon: FaNode, color: "#008000" },
+  { name: "React.js", icon: FaReact, color: "#ADD8E6" },
+  { name: "MongoDB", icon: FaDatabase, color: "#008000" },
+  { name: "Tailwind CSS", icon: FaCode, color: "#00FFFF" },
+  { name: "Redux.js", icon: FaCode, color: "#800080" },
+  { name: "Express", icon: FaCode, color: "#808080" },
+  { name: "TypeScript", icon: FaCode, color: "#0000FF" },
+  { name: "VS Code", icon: FaCode, color: "#0000FF" },
+  { name: "Git", icon: FaGit, color: "#FF0000" },
+  { name: "GitHub", icon: FaGithub, color: "#800080" },
+  { name: "Netlify", icon: FaCode, color: "#000000" },
+  { name: "Figma", icon: FaFigma, color: "#FFC0CB" },
+  { name: "Chrome", icon: FaChrome, color: "#FF0000" },
 ];
 
 const SkillCard = ({ skill }) => {
@@ -37,7 +37,7 @@ const SkillCard = ({ skill }) => {
   return (
     <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 p-4">
       <div
-        className={`bg-${skill.color} shadow-lg rounded-lg overflow-hidden transition duration-500 ease-in-out transform hover:bg-${skill.color}-700 hover:scale-105`}
+        className={`bg-${skill.color} shadow-lg rounded-lg overflow-hidden transition duration-300 ease-in-out transform hover:bg-${skill.color}-700 hover:scale-105 hover:shadow-xl`}
       >
         <div className="px-6 py-4 bg-[#14083b]">
           <div className="flex justify-center">
@@ -54,7 +54,7 @@ const SkillCard = ({ skill }) => {
 
 const SkillsCollection = () => {
   return (
-    <div className="bg-gray-600 w-full sm:w-1/3 md:w-1/2 lg:w-full xl:w-full">
+    <div className="bg-gray-600 w-full p-4">
       <h1 className="text-4xl font-bold text-center py-8 text-white">
         My <span className="text-blue-600">Skills</span>
       </h1>
